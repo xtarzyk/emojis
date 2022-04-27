af7c6bce4660b1e0e682cbebf6b75d58291a7f5b
xtarzyk <xtarzyk@tutanota.com>
2022-04-27 11:42:07 +0200

    done

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const emojis = document.querySelectorAll('.emoji')
+const visibleEmojis = document.querySelector('.emojis__slider')
 const slider = document.querySelector('.slider__content')
 const prevBtn = document.querySelector('.prev')
 const nextBtn = document.querySelector('.next')
-
-console.log(emojis)
+const result = document.querySelector('.result')
+let emojisMap = new Map()
 
 const moveRight = () => {
     if (slider.classList.contains('slide-to-center')) {
@@ -39,5 +40,30 @@ const moveLeft = () => {
     slider.classList.add('slide-to-left')
 }
 
+const render = () => {
+    const emojiKeys = Array.from(emojisMap.values())
+    result.replaceChildren(...emojiKeys)
+}
+
+const addEmoji = event => {
+    const newP = document.createElement('p')
+    newP.textContent = event.target.textContent
+    newP.classList.add(...event.target.classList)
+
+    if (event.target.classList.contains('emoji')) {
+        emojisMap.set(event.target.textContent, newP)
+    }
+}
+
 prevBtn.addEventListener('click', moveLeft)
 nextBtn.addEventListener('click', moveRight)
+
+visibleEmojis.addEventListener('click', event => {
+    addEmoji(event)
+    render()
+})
+
+result.addEventListener('click', event => {
+    result.removeChild(event.target)
+    emojisMap.delete(event.target.textContent)
+})
